4b509d1cf99949c6408944907854f41c834f253d
EduardoModel <edu_model@hotmail.com>
2022-01-28 20:23:46 +0100

    Ensure Encrypter calls bcrypt with correct values

--- src/utils/encrypter.spec.ts
+++ src/utils/encrypter.spec.ts
@@ -27,10 +27,18 @@ describe('Encrypter', () => {
     const isValid = await sut.compare('any_string', 'hashed_string')
     expect(isValid).toBe(true)
   })
+
   test('it should return false if bcrypt returns false', async () => {
     const { sut } = makeSut()
     bcrypt.isHashValid = false
     const isValid = await sut.compare('any_string', 'hashed_string')
     expect(isValid).toBe(false)
+  })
+
+  test('it should call bcrypt with correct values', async () => {
+    const { sut } = makeSut()
+    await sut.compare('any_string', 'hashed_string')
+    expect(bcrypt.s).toBe(sut.string)
+    expect(bcrypt.hash).toBe(sut.hash)
   })
 })
