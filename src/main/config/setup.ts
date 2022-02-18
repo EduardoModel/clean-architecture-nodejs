ea12a1bcd7223f511a8f694ff0d602f5d7d67bfb
EduardoModel <edu_model@hotmail.com>
2022-02-18 16:53:10 +0100

    Move express app configuration to a other file

---
+++ src/main/config/setup.ts
@@ -0,0 +1,4 @@
+
+export default (app) => {
+  app.disable('x-powered-by')
+}
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import setupApp from './config/setup'
+
 const app = express()
+setupApp(app)
 
 app.listen(8000, () => console.log('Server is running on port 8000'))
