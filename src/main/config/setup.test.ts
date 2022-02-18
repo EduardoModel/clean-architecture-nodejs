c5701573fb46a7411ebc11a7bbf590d567669d7a
EduardoModel <edu_model@hotmail.com>
2022-02-18 17:26:36 +0100

    Enable all CORS, Methods and Headers

--- src/main/config/setup.test.ts
+++ src/main/config/setup.test.ts
@@ -10,4 +10,15 @@ describe('AppSetup', () => {
 
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('It should enable CORS', async () => {
+    app.get('/test_x_powered_by', (req, res) => {
+      res.send('')
+    })
+    const res = await supertest(app).get('/test_x_powered_by')
+
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
