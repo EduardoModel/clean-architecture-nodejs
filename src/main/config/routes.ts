a08addc2eb4e614938f6864576e47233d9b4fb19
EduardoModel <edu_model@hotmail.com>
2022-02-22 13:38:24 +0100

    Added routes files

---
+++ src/main/config/routes.ts
@@ -0,0 +1,16 @@
+/*
+  To avoid this file to grow exponentialy as the application grows, it will be only
+  responsable to get the routes scenarios from the routes folder and link them to the properly endpoints
+*/
+import express from 'express'
+import fb from 'fast-glob'
+const router = express.Router()
+
+export default (app) => {
+  // Set the prefix for the routes that will be added
+  app.use('/api', router)
+  fb.sync('**/src/main/routes/**.ts').forEach(async file => {
+    const route = await import(`../../../${file}`)
+    route.default(router)
+  })
+}
